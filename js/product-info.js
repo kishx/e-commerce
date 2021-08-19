23962500a3f7e041f60e9926fc7fd73821cc36c1
kishx <camibernadet@gmail.com>
2021-08-19 20:12:14 -0300

    Revert "commit 2"
    This reverts commit e3da32f016285d6493bfeac94e11142ef821a044.

--- js/product-info.js
+++ js/product-info.js
@@ -1,43 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-var product = {};
-
-function showImagesGallery(array){
-
-    let htmlContentToAppend = "";
-
-    for(let i = 0; i < array.length; i++){
-        let imageSrc = array[i];
-
-        htmlContentToAppend += `
-        <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
-            </div>
-        </div>
-        `
-
-        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            product = resultObj.data;
 
-            let productNameHTML  = document.getElementById("productName");
-            let productDescriptionHTML = document.getElementById("productDescription");
-            let productCriteriaHTML = document.getElementById("productCriteria");
-        
-            productNameHTML.innerHTML = product.name;
-            productDescriptionHTML.innerHTML = product.description;
-            productCriteriaHTML.innerHTML = product.productCriteria;
-
-            //Muestro las imagenes en forma de galería
-            showImagesGallery(product.images);
-        }
-    });
 });
--- js/products.js
+++ js/products.js
@@ -1,47 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-var productsArray = []; 
-
-
-function showProductsList(){
-
-    let htmlContentToAppend = "";
-    for(let i = 0; i < productsArray.length; i++){
-        let product = productsArray[i];
-
-            htmlContentToAppend += `
-            <divclass="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
-                        </div>
-                        <p>` + product.description + `</p>
-                    </div>
-                </div>
-            </div>
-            `
-        }
-
-        document.getElementById("product").innerHTML = htmlContentToAppend;
-    }
-
-
-
-
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        
-        {
-            productsArray = resultObj.data
-            showProductsList(productsArray);
-        }
-    });
-
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
