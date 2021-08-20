d0da04c8fc92443a6431bd786c2d42e5b4bb3b19
kishx <camibernadet@gmail.com>
2021-08-20 12:02:14 -0300

    commit 2-

--- js/products.js
+++ js/products.js
@@ -1,6 +1,5 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+});
 var productsArray = []; 
 
 
@@ -11,7 +10,7 @@ function showProductsList(){
         let product = productsArray[i];
 
             htmlContentToAppend += `
-            <divclass="list-group-item list-group-item-action">
+            <div class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
                         <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
@@ -19,6 +18,7 @@ function showProductsList(){
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount +  ` Vendidos </small>
                         </div>
                         <p>` + product.description + `</p>
                     </div>
