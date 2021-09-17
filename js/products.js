e092dcd8a0abcdd6e54ae8520595fb2588bc6eff
kishx <camibernadet@gmail.com>
2021-09-17 14:01:45 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -47,8 +47,7 @@ function showProductsList() {
 
 
         htmlContentToAppend += `
-        <a href="product-info.html">
-            <div class="list-group-item list-group-item-action">
+        <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
                         <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
